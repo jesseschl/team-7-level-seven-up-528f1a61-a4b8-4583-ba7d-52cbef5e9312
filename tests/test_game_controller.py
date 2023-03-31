0dfb0b7bc52c44373fc59c2e7f8c1df3f4581902
Jesse <jesseschloess@gmail.com>
2023-03-31 18:04:29 +0000

    Bubbles fillin. trying my best

--- tests/test_game_controller.py
+++ tests/test_game_controller.py
@@ -5,6 +5,11 @@ from levelup.character import DEFAULT_CHARACTER_NAME, InvalidMoveException, Char
 from levelup.map import Direction
 from levelup.position import Position
 
+class FakeCharacter(Character):
+    name: str
+
+    def __init__(self, name = DEFAULT_CHARACTER_NAME):
+        self.name = name
 
 class TestGameController(TestCase):
     def test_init(self):
@@ -14,3 +19,9 @@ class TestGameController(TestCase):
             testobj.status.current_position,
             None,
         )
+
+    def test_create_character(self):
+        testobj = GameController()
+        test_char = FakeCharacter("arbitrary")
+        testobj.create_character("arbitrary")
+        self.assertEqual(testobj.character.name, test_char.name)
